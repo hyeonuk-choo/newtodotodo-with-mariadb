39ce48b9d1f09bf5289bd513317f6efcb2599fc5
hyeonuk-choo <hyeonuk97@naver.com>
2023-05-13 11:14:30 +0900

    fix: nth-check 취약성관련 디버깅
    nth-check 취약성관련 디버깅
    불필요한 모듈 제거

--- src/redux/modules/statisticsSlice.jsx
+++ src/redux/modules/statisticsSlice.jsx
@@ -4,19 +4,6 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const token = localStorage.getItem("token");
 
-export const __getLineChartData = createAsyncThunk(
-  "getLineChartData",
-  async (payload, thunkAPI) => {
-    try {
-      const data = await axios.get(`${BASE_URL}/achievement/thisweek`);
-
-      return thunkAPI.fulfillWithValue(data.data);
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.code);
-    }
-  }
-);
-
 export const getRank = createAsyncThunk(
   "getRank",
   async (payload, thunkAPI) => {
@@ -70,17 +57,6 @@ export const statisticsSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(__getLineChartData.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(__getLineChartData.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.lineData = action.payload;
-      })
-      .addCase(__getLineChartData.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload.message;
-      })
       .addCase(getRank.pending, (state) => {
         state.isLoading = true;
       })
