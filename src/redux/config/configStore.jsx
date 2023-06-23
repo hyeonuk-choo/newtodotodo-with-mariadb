6b6c2e63df99be9b6f19d9ef33382dc6741fa4c4
hyeonuk-choo <hyeonuk97@naver.com>
2023-06-23 16:07:26 +0900

    #6 fix: 중앙상태값 초기화방지
    유저가 새로고침시 중앙상태값이 초기화 되는 것을 방지

--- src/redux/config/configStore.jsx
+++ src/redux/config/configStore.jsx
@@ -1,16 +1,45 @@
 // 리덕스툴킷 스토어함수
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 // 리듀서
 import mainSlice from "../modules/mainSlice";
 import statisticsSlice from "../modules/statisticsSlice";
 import PlannerSlice from "../modules/plannerSlice";
 import loginSlice from "../modules/loginSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  login: loginSlice,
+  main: mainSlice,
+  statistics: statisticsSlice,
+  planner: PlannerSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    login: loginSlice,
-    main: mainSlice,
-    statistics: statisticsSlice,
-    planner: PlannerSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
